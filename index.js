8db8379e2cd40ceca7babcd57798ff4a3951299c
Jawwad Umar <jawwadumar99@gmail.com>
2024-06-05 02:44:52 +0530

    speed and position added

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const button = document.getElementById('start')
+const speedParagraph = document.getElementById("currentspeed");
+const locationParagraph = document.getElementById("location");
+
 
 const successCallback = (position)=>{
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   const speed = position.coords.speed;
 
-  const speedParagraph = document.getElementById("currentspeed");
+  
   speedParagraph.textContent = `${Math.round(speed)} km/h`;
-
-  const locationParagraph = document.getElementById("location");
   locationParagraph.textContent = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
 
 }
@@ -21,7 +22,23 @@ const options = {
     enableHighAccuracy: true
 };
 
-
+let watchId = null
 button.addEventListener('click', (event)=>{
-    const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+
+    if(!watchId){
+        watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, options); //this updates speed and postition automatically
+        
+        button.textContent = '🛑 Stop';
+        button.classList.toggle('selected');
+    }
+    else{
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+        speedParagraph.textContent = "0 km/h";
+        locationParagraph.textContent = "Click on the Start Button to track location";
+        button.textContent = '🔑 Start';
+        button.classList.toggle('selected');
+
+    }
+     
 })    
